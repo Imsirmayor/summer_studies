24544ab092ec70689eb6dcc540c5fcbfc4368ac8
Imsirmayor <m.adeyem.e@gmail.com>
2024-07-31 22:22:10 +0200

    band generator code

---
+++ Day 01/band_genrator_name.py
@@ -0,0 +1,8 @@
+"""
+This code concatinates two strings together to give a band name
+"""
+print("Welcome to the band name generator")
+city_you_grew_up = input("what's the name of the city you grew up\n")
+pet_name = input("what's Pet's name\n")
+print("Your band Name Could BE")
+print(f'{city_you_grew_up} {pet_name}')
